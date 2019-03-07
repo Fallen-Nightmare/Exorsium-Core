ed7eeb0dc756d2dcb401c44de288c014dcd620d6
EpsiloneDev <reddust.contact@gmail.com>
2019-03-07 10:00:42 +0100

    dressnpc apply

--- src/server/game/Handlers/NPCHandler.cpp
+++ src/server/game/Handlers/NPCHandler.cpp
@@ -61,6 +61,11 @@ void WorldSession::HandleTabardVendorActivateOpcode(WorldPacket& recvData)
     ObjectGuid guid;
     recvData >> guid;
 
+#ifndef DISABLE_DRESSNPCS_CORESOUNDS
+    if (guid.IsAnyTypeCreature())
+        if (Creature* creature = _player->GetMap()->GetCreature(guid))
+            creature->SendMirrorSound(_player, 0);
+#endif
     Creature* unit = GetPlayer()->GetNPCIfCanInteractWith(guid, UNIT_NPC_FLAG_TABARDDESIGNER);
     if (!unit)
     {
@@ -90,6 +95,11 @@ void WorldSession::HandleBankerActivateOpcode(WorldPacket& recvData)
 
     recvData >> guid;
 
+#ifndef DISABLE_DRESSNPCS_CORESOUNDS
+    if (guid.IsAnyTypeCreature())
+        if (Creature* creature = _player->GetMap()->GetCreature(guid))
+            creature->SendMirrorSound(_player, 0);
+#endif
     Creature* unit = GetPlayer()->GetNPCIfCanInteractWith(guid, UNIT_NPC_FLAG_BANKER);
     if (!unit)
     {
@@ -127,6 +137,10 @@ void WorldSession::HandleTrainerListOpcode(WorldPackets::NPC::Hello& packet)
         TC_LOG_DEBUG("network", "WorldSession: SendTrainerList - %s not found or you can not interact with him.", packet.Unit.ToString().c_str());
         return;
     }
+
+#ifndef DISABLE_DRESSNPCS_CORESOUNDS
+    npc->SendMirrorSound(_player, 0);
+#endif
 
     SendTrainerList(npc);
 }
@@ -182,6 +196,11 @@ void WorldSession::HandleGossipHelloOpcode(WorldPacket& recvData)
     ObjectGuid guid;
     recvData >> guid;
 
+#ifndef DISABLE_DRESSNPCS_CORESOUNDS
+    if (guid.IsAnyTypeCreature())
+        if (Creature* creature = _player->GetMap()->GetCreature(guid))
+            creature->SendMirrorSound(_player, 0);
+#endif
     Creature* unit = GetPlayer()->GetNPCIfCanInteractWith(guid, UNIT_NPC_FLAG_GOSSIP);
     if (!unit)
     {
@@ -323,6 +342,11 @@ void WorldSession::HandleListStabledPetsOpcode(WorldPacket& recvData)
 
     recvData >> npcGUID;
 
+#ifndef DISABLE_DRESSNPCS_CORESOUNDS
+    if (npcGUID.IsAnyTypeCreature())
+        if (Creature* creature = _player->GetMap()->GetCreature(npcGUID))
+            creature->SendMirrorSound(_player, 0);
+#endif
     if (!CheckStableMaster(npcGUID))
         return;
 
